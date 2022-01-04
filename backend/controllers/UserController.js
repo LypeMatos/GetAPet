3c93bf8f4deb20a023238603a30fc90b313d0401
Felipe <lype_matos@hotmail.com>
2022-01-04 19:22:58 -0300

    atualização

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,7 +1,67 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const createUserToken = require('../helpers/create-user-token');
 
 module.exports = class UserController {
-    static async register(req, res){
-        res.json('Olá Get A Pet');
+    static async register(req, res) {
+        const { name, email, phone, password, confirmpassword } = req.body;
+
+        //validations
+        if (!name) {
+            res.status(422).json({ message: 'O nome é obrigatório!' });
+            return;
+        }
+
+        if (!email) {
+            res.status(422).json({ message: 'O e-mail é obrigatório!' });
+            return;
+        }
+
+        if (!phone) {
+            res.status(422).json({ message: 'O telefone é obrigatório!' });
+            return;
+        }
+
+        if (!password) {
+            res.status(422).json({ message: 'A senha é obrigatório!' });
+            return;
+        }
+
+        if (!confirmpassword) {
+            res.status(422).json({ message: 'A confirmação de senha é obrigatório!' });
+            return;
+        }
+
+        if (password !== confirmpassword) {
+            res.status(422).json({ message: 'As senhas não conferem!' });
+            return;
+        }
+
+        //check if user exists
+        const userExists = await User.findOne({ email: email });
+
+        if (userExists) {
+            res.status(422).json({ message: 'E-mail já cadastrado. Por favor utilizar outro e-mail!' });
+            return;
+        }
+
+        //create a password
+        const salt = await bcrypt.genSalt(12);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        //create a user
+        const user = new User({
+            name: name,
+            email: email,
+            phone: phone,
+            password: passwordHash
+        })
+
+        try {
+            const newUser = await user.save();
+            await createUserToken(newUser, req, res);
+        } catch (error) {
+            res.status(500).json({message: error});
+        }
     }
 }
